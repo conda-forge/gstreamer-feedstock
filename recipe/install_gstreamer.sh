675d7bc59b6cfac01f3f1536a3354654cfbcd795
Ray Donnelly <mingw.android@gmail.com>
2018-04-10 19:09:38 +0100

    Enable gobject-introspection

--- recipe/install_gstreamer.sh
+++ recipe/install_gstreamer.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# gstreamer expects libffi to be in lib64 for some reason.
-[[ -d $PREFIX/lib64 ]] || mkdir $PREFIX/lib64
-cp -r $PREFIX/lib/libffi* $PREFIX/lib64
-
 # The datarootdir option places the docs into a temp folder that won't
 # be included in the package (it is about 12MB).
 
@@ -13,9 +9,10 @@ export M4="${BUILD_PREFIX}/bin/m4"
 # --disable-examples because:
 # https://bugzilla.gnome.org/show_bug.cgi?id=770623#c16
 # http://lists.gnu.org/archive/html/libtool/2016-05/msg00022.html
-./configure --prefix="$PREFIX"    \
-            --disable-examples    \
-            --disable-benchmarks  \
+./configure --prefix="$PREFIX"     \
+            --disable-examples     \
+            --disable-benchmarks   \
+            --enable-introspection \
             --datarootdir=`pwd`/tmpshare
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
@@ -24,7 +21,3 @@ make -j${CPU_COUNT} ${VERBOSE_AT}
 # why that's happening though.
 # make check
 make install
-
-# Remove the created lib64 directory
-rm -rf $PREFIX/lib64
-
