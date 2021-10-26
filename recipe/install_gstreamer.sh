757f246751e51cd9efefaaed486a2fa5de90323d
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-10-26 11:28:39 -0500

    Add cross-python

--- recipe/install_gstreamer.sh
+++ recipe/install_gstreamer.sh
@@ -5,12 +5,6 @@ pushd build
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
 
-if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
-  # Meson pulls arm64 python as part of its distribution, which cannot be executed on x86_64 CIs
-  CONDA_SUBDIR="osx-64" conda create -y --prefix "${SRC_DIR}/osx_64_python" python -c conda-forge
-  export PATH="${SRC_DIR}/osx_64_python/bin":$PATH
-fi
-
 meson --prefix=${PREFIX} \
       --buildtype=release \
       --libdir=$PREFIX/lib \
