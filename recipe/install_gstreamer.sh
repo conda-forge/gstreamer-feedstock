19b76c317b60a1d162308d4997db802b033157e9
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-10-27 17:55:52 -0500

    Disable introspection since g-ir-scanner will produce x86_64 binaries

--- recipe/install_gstreamer.sh
+++ recipe/install_gstreamer.sh
@@ -4,14 +4,14 @@ mkdir build
 pushd build
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
-EXTRA_FLAGS=""
+EXTRA_FLAGS="-Dintrospection=enabled"
 if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
   # Add pkg-config to cross-file binaries since meson will disable it
   # See https://github.com/mesonbuild/meson/issues/7276
   echo "[binaries]" >> $BUILD_PREFIX/meson_cross_file.txt
   echo "pkg-config = '$(which pkg-config)'" >> $BUILD_PREFIX/meson_cross_file.txt
   # Use Meson cross-file flag to enable cross compilation
-  EXTRA_FLAGS="--cross-file $BUILD_PREFIX/meson_cross_file.txt"
+  EXTRA_FLAGS="--cross-file $BUILD_PREFIX/meson_cross_file.txt -Dintrospection=disabled"
 fi
 
 export PKG_CONFIG=$(which pkg-config)
@@ -20,7 +20,6 @@ meson --prefix=${PREFIX} \
       --buildtype=release \
       --libdir=$PREFIX/lib \
       $EXTRA_FLAGS \
-      -Dintrospection=enabled \
       -Dptp-helper-permissions=none \
       -Dexamples=disabled \
       -Dtests=disabled \
