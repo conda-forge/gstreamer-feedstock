8f4da2cab8cc1df2238554598186409a13aed3fc
Mark Harfouche <mark.harfouche@gmail.com>
2023-04-12 00:43:31 -0400

    Update install_base_plugins.sh

--- recipe/install_base_plugins.sh
+++ recipe/install_base_plugins.sh
@@ -9,14 +9,6 @@ pushd build
 
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig:$BUILD_PREFIX/lib/pkgconfig
 EXTRA_FLAGS="-Dintrospection=enabled"
-if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" ]]; then
-  # Add pkg-config to cross-file binaries since meson will disable it
-  # See https://github.com/mesonbuild/meson/issues/7276
-  echo "[binaries]" >> $BUILD_PREFIX/meson_cross_file.txt
-  echo "pkg-config = '$(which pkg-config)'" >> $BUILD_PREFIX/meson_cross_file.txt
-  # Use Meson cross-file flag to enable cross compilation
-  EXTRA_FLAGS="--cross-file $BUILD_PREFIX/meson_cross_file.txt -Dintrospection=disabled"
-fi
 
 export PKG_CONFIG=$(which pkg-config)
 
@@ -33,7 +25,8 @@ if [ -n "$OSX_ARCH" ] ; then
 	meson_options+=(-Dxshm=disabled)
 fi
 
-meson --prefix=${PREFIX} \
+meson ${MESON_ARGS} \
+      --prefix=${PREFIX} \
       --libdir=$PREFIX/lib \
       --buildtype=release \
       $EXTRA_FLAGS \
